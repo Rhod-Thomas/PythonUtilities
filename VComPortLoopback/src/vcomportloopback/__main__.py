29ee642e0bcbfe3fe696cd254f5037b6031521f5
Rhod-Thomas <engineer_rod@hotmail.co.uk>
2026-03-02 19:54:24 +0000

    initial commit for project VComPortLoopback

---
+++ VComPortLoopback/src/vcomportloopback/__main__.py
@@ -0,0 +1,6 @@
+##import modules to run here
+from .serialLoopback import test_loopback
+
+if __name__ == "__main__":
+    #entry point for the program
+    test_loopback()
---
+++ VComPortLoopback/src/vcomportloopback/serialLoopback.py
@@ -0,0 +1,9 @@
+##Com port loopback test file. 
+
+import serial as ser
+
+def test_loopback():
+    print("Test loopback function executed")
+    
+def test_others():
+    print("Test others function executed")
